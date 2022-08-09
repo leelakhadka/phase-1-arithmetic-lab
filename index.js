63d1d5638f25ba7437700fe12df07fede085ddfb
leela khadka <khadkaleela3@gmail.com>
2022-08-08 21:44:03 -0700

    commit tree 5cf78c77012a208a9031c0154d9ebb73509513d3

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
 
-// Write your code here
+var random = Math.floor(Math.random() * 10) + 1;
+
+var num3 = 4;
+var num4 = 1;
+
+var mod = 4 / 1;
+
+var max = Math.max(1, 2, 3, 4, 5, 20);
